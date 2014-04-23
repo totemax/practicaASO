2e250ac1834c8409aa9ff05a63df3939b43e3264
totemax <jjorgeserran@gmail.com>
2014-04-23 14:04:43 +0000

    Añadida la cabecera de menu

--- gestiona-prac.sh
+++ gestiona-prac.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+echo 'ASO 13/14 - Práctica 5';
+echo 'Jesús Jorge Serrano';
+
+echo ;
+echo ;
+echo 'Herramienta de gestión de prácticas';
+echo '-----------------------------------';
