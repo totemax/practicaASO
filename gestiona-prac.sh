af18d861d3e345a9d2fe5a0d46d084bd9847e5c9
totemax <jjorgeserran@gmail.com>
2014-04-24 14:06:52 +0000

    Menu function

--- gestiona-prac.sh
+++ gestiona-prac.sh
@@ -3,7 +3,27 @@
 echo 'ASO 13/14 - Práctica 5';
 echo 'Jesús Jorge Serrano';
 
-echo ;
-echo ;
-echo 'Herramienta de gestión de prácticas';
-echo '-----------------------------------';
+echo 
+echo 
+echo 'Herramienta de gestión de prácticas'
+echo '-----------------------------------'
+
+#### Functions #####
+
+displayMenu(){
+    echo 'Menú'
+    case $1 in 
+        0)
+            echo '1) Programar recogida de prácticas'
+            echo '2) Empaquetar prácticas de una asignatura'
+            echo '3) Ver tamaño y fecha del fichero de la asignatura'
+            echo '4) Enviar un backup de prácticas a un servidor remoto'
+            echo '5) Finalizar programa'
+        ;;
+    esac
+    read -p "Opción:" opcion
+    return $opcion
+}
+
+opcion=displayMenu 0
+echo $opcion
